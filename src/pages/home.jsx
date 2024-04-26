7d007c93ba21516515398e91548a8fec5c8f8c71
FRANCISCO GASPERINI RIBAS <francisco_ribas@estudante.sesisenai.org.br>
2024-04-26 08:04:58 -0300

    certo2

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -8,7 +8,11 @@ function Home() {
             <h1>
                 Salve Salve Consagrado   
             </h1>
+
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/wfaWGFl0XBQ?si=AzjHzguXBvCpHfHx" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
             </div>
+
+            
     )
 }
 
