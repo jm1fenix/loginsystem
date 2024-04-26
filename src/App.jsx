7b74cd8e2b1e00f8f935c6f0fbed3891d2e3b58b
FRANCISCO GASPERINI RIBAS <francisco_ribas@estudante.sesisenai.org.br>
2024-04-26 07:59:13 -0300

    certino papae

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,24 @@
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Cadastro from './pages/cadastro';
+import Home from './pages/home';
+import Login from './pages/login';
 import axios from 'axios';
 
-const logar = async (username, password) => {
-  try {
-  const response = await axios.post('http://localhost:8090/api/login', {
-  username: username,
-  password: password,
-  });
-  return response.data;
-  } catch (error) {
-  throw error;
+function App() {
+    return (
+      <Router>
+        {}
+        <Routes>
+          {}
+          <Route path="/Home" element={<Home />} />        
+         {}
+          <Route path="/Cadastro" element={<Cadastro />} />
+          {}
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </Router>
+    );
   }
- };
- function App() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const handleLogin = async () => {
-  try {
-  const response = await logar(username, password);
-  alert(response);
-  } catch (error) {
-  console.error('Erro ao se logar:', error);
-  }
-  };
-  return (
-  <div>
-  <h1>Sistema de Login</h1>
-  <form>
-  <label>
-  Usuário: 
- <br /> <input
-  type="text"
-  value={username}
-  onChange={(e) => setUsername(e.target.value)}
-  /> <br />
-  </label>
-  <br />
-  <label>
-  Senha: 
-  <br /><input
-  type="password"
-  value={password}
-  onChange={(e) => setPassword(e.target.value)}
-  />
-  </label>
-  <br /><br />
-  <button type="button" onClick={handleLogin}>
-  Login
-  </button>
-  </form>
-  </div>
-  );
- }
- export default App;
- 
+  export default App;
+  
